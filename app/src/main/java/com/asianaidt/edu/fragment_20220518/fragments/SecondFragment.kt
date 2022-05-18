dfafe8ddb9e04d7f6a10fb5afa2a4a8d05ab85a7
Jin <lycanjin@gmail.com>
2022-05-18 13:07:35 +0900

    Jin 1. 2번째 fragment 배치 실습

---
+++ app/src/main/java/com/asianaidt/edu/fragment_20220518/fragments/SecondFragment.kt
@@ -0,0 +1,32 @@
+package com.asianaidt.edu.fragment_20220518.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.asianaidt.edu.fragment_20220518.R
+import kotlinx.android.synthetic.main.fragment_first.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btn_log.setOnClickListener {
+
+            Log.d("프래그먼트", "프래그먼트에서의 로그")
+
+        }
+    }
+
+}
